b2b36bf423ded21468da6f71e093f8dd680ea298
Lars van Kleij <larsvankleij@gmail.com>
2022-11-28 12:18:54 +0100

    Initial commit

---
+++ Advent of Code/Reader/Reader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Reader
+{
+    public class Reader
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+        public static void SetIn(int day)
+        {
+            string path = "..\\..\\..\\Day" + day + "\\in.txt";
+            Console.SetIn(new StreamReader(path));
+        }
+    }
+}
