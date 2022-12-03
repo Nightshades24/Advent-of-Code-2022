547b856b5b09cb6557cdc7b321cd53a8e9cbb0e6
Lars van Kleij <larsvankleij@gmail.com>
2022-12-03 11:38:33 +0100

    Day4

---
+++ Day4/Program.cs
@@ -0,0 +1,19 @@
+﻿using Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader.Reader.SetIn(4);
+
+            Console.ReadKey();
+        }
+    }
+}
