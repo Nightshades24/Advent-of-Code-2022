a52569a24d9fcf2470389ac8315054ecee3b9611
Lars van Kleij <larsvankleij@gmail.com>
2022-12-05 10:45:49 +0100

    Day4 & Day5

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -9,9 +9,51 @@ namespace Day4
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Reader.Reader.SetIn(4);
+
+            
+            int counter1 = 0;
+            int counter2 = 0;
+            string temp = Console.ReadLine();
+            string[] input = temp.Split(',');
+            
+            while (temp != null)
+            {
+                int[] pair1 = Array.ConvertAll(input[0].Split('-'), s => int.Parse(s));
+                int[] pair2 = Array.ConvertAll(input[1].Split('-'), s => int.Parse(s));
+
+                if (pair1[0] <= pair2[0] && pair1[1] >= pair2[1])
+                {
+                    counter1++;
+                }
+                else if (pair1[0] >= pair2[0] && pair1[1] <= pair2[1])
+                {
+                    counter1++;
+                }
+                else if (pair1[0] <= pair2[0] && pair2[0] <= pair1[1])
+                {
+                    counter2++;
+                }
+                else if (pair2[0] <= pair1[0] && pair1[0] <= pair2[1])
+                {
+                    counter2++;
+                }
+                else if (pair1[0] <= pair2[1] && pair2[1] <= pair1[1])
+                {
+                    counter2++;
+                }
+                else if (pair2[0] <= pair1[1] && pair1[1] <= pair2[1])
+                {
+                    counter2++;
+                }
+
+                temp = Console.ReadLine();
+                if (temp != null) input = temp.Split(',');
+            }
+            Console.WriteLine("Part1: " + counter1);
+            Console.WriteLine("Part2: " + (counter1 + counter2));
 
             Console.ReadKey();
         }
---
+++ Day5/Program.cs
@@ -0,0 +1,81 @@
+﻿using Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Day5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Reader.Reader.SetIn(5);
+
+            string[] towers = new string[9];
+
+            string input = Console.ReadLine();
+            while (!input.Contains("1"))
+            {
+                if (input[1] != ' ') towers[0] += (input[1]);
+                if (input[5] != ' ') towers[1] += (input[5]);
+                if (input[9] != ' ') towers[2] += (input[9]);
+                if (input[13] != ' ') towers[3] += (input[13]);
+                if (input[17] != ' ') towers[4] += (input[17]);
+                if (input[21] != ' ') towers[5] += (input[21]);
+                if (input[25] != ' ') towers[6] += (input[25]);
+                if (input[29] != ' ') towers[7] += (input[29]);
+                if (input[33] != ' ') towers[8] += (input[33]);
+
+                input = Console.ReadLine();
+            }
+
+            while (!input.Contains("move"))
+                input = Console.ReadLine();
+
+            for (int i = 0; i < towers.Length; i++)
+            {
+                char[] charArray = towers[i].ToCharArray();
+                Array.Reverse(charArray);
+                towers[i] = new string(charArray);
+            }
+
+            while (input != null)
+            {
+                string[] move = input.Split();
+                Stack<char> crates = new Stack<char>();
+                int from = int.Parse(move[3]) - 1;
+                int to = int.Parse(move[5]) - 1;
+
+                for (int i = 0; i < int.Parse(move[1]); i++)
+                {
+                    // PART1: char crate = towers[from][towers[from].Length - 1];
+                    crates.Push(towers[from][towers[from].Length - 1]);
+
+                    // PART1: towers[to] += crate;
+                    towers[from] = towers[from].Remove(towers[from].Length - 1);
+                }
+
+                for (int i = 0; i < int.Parse(move[1]); i++)
+                {
+                    char crate = crates.Pop();
+
+                    towers[to] += crate;
+                }
+                input = Console.ReadLine();
+            }
+
+            string result = "";
+            for (int i = 0; i < towers.Length; i++)
+            {
+                result += towers[i][towers[i].Length - 1];
+            }
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
