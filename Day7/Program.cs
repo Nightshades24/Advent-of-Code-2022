f08dfdc7b3394999c402a3e2600d2b1f4bcea7c7
Lars van Kleij <larsvankleij@gmail.com>
2022-12-06 07:45:40 +0100

    Day7

---
+++ Day7/Program.cs
@@ -0,0 +1,19 @@
+﻿using Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Reader.Reader.SetIn(7);
+
+            Console.ReadKey();
+        }
+    }
+}
