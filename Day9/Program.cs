747cc308bb3378b1b40de8cca3ba33ab30493694
Lars van Kleij <larsvankleij@gmail.com>
2022-12-08 09:50:11 +0100

    Day9

---
+++ Day9/Program.cs
@@ -0,0 +1,21 @@
+﻿using Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Reader.Reader.SetIn(9);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
