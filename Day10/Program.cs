43735a0e2cffe4bde384991fb4ce0b29f886396c
Lars van Kleij <larsvankleij@gmail.com>
2022-12-09 14:33:59 +0100

    Day10

---
+++ Day10/Program.cs
@@ -0,0 +1,19 @@
+﻿using Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Reader.Reader.SetIn(10);
+
+            Console.ReadKey();
+        }
+    }
+}
