4f4ba168d5ed3f75ae411034133aa212abecc36b
Lars van Kleij <larsvankleij@gmail.com>
2022-12-10 08:18:09 +0100

    Day10

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -13,6 +13,62 @@ namespace Day10
         {
             Reader.Reader.SetIn(10);
 
+            int signalstrength = 0;
+            int X = 1;
+            int cycle = 0;
+            bool add = false;
+            string answer = "";
+
+            string temp = Console.ReadLine();
+
+            while (temp != null)
+            {
+                if (cycle % 40 == (X-1) || cycle % 40 == X || cycle % 40 == (X + 1))
+                {
+                    answer += "#";
+                }
+                else
+                {
+                    answer += ".";
+                }
+
+                cycle++;
+                
+                if (cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220)
+                {
+                    //Console.WriteLine($"During the {cycle}th cycle, register X has the value {X}, so the signal strength is {cycle} * {X} = " + (cycle * X));
+                    signalstrength += cycle * X;
+                }
+
+
+                string[] input = temp.Split(' ');
+
+                if (input[0] == "noop")
+                {
+                    temp = Console.ReadLine();
+                }
+                else if (input[0] == "addx" && !add)
+                {
+                    add = true;
+                }
+                else if (input[0] == "addx" && add)
+                {
+                    add = false;
+                    X += int.Parse(input[1]);
+                    temp = Console.ReadLine();
+                }
+
+            }
+
+
+            Console.WriteLine("Part1: " + signalstrength);
+            Console.WriteLine("\nPart2:");
+            for (int i = 0; i < 240; i++)
+            {
+                if (i % 40 == 0) Console.WriteLine();
+                Console.Write(answer[i]);
+            }
+
             Console.ReadKey();
         }
     }
